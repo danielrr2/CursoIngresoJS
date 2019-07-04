b89a0052bde2066ef82b25b9006cb5e0a5827041
danielrr2 <52512211+danielrr2@users.noreply.github.com>
2019-07-03 21:56:48 -0300

    Actualizado 10:00

--- 1-EntradaSalida/jsEntradaSalida-1.js
+++ 1-EntradaSalida/jsEntradaSalida-1.js
@@ -1,6 +1,9 @@
 //Debemos lograr mostrar un mensaje al presionar el botón  'mostrar'.
 function mostrar()
 {
-	alert("ok");
+	var nombre;
+	
+	alert("Esto funciona de maravilla");
+	nombre= prompt("ingrese nombre") //alert Muestra una ventana.
+	
 }
-
