c8c741fdb1ef85328a7884bb12c738efdfc90f14
danielrr2 <52512211+danielrr2@users.noreply.github.com>
2019-07-05 21:30:20 -0300

    05 julio

--- 1-EntradaSalida/jsEntradaSalida-9.js
+++ 1-EntradaSalida/jsEntradaSalida-9.js
@@ -3,7 +3,16 @@ transformarlo a entero (parseInt), luego
 mostrar el importe con un aumento del 10 %
 en el cuadro de texto "RESULTADO".*/
 function mostrarAumento()
-{
+{var sueldo;
+    var aumento;
+    var nuevosueldo;
+    sueldo=document.getElementById("sueldo").value;
+    sueldo=parseInt(sueldo);
+    aumento=sueldo*10/100;
+    nuevosueldo=(sueldo+aumento);
 
-	
+    
+    document.getElementById("resultado").value=nuevosueldo;
+ 
+   
 }
