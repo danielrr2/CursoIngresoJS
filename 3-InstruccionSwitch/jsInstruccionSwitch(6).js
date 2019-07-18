fe5c6db9b01683b6db72db86309d4b9ec908db00
danielrr2 <52512211+danielrr2@users.noreply.github.com>
2019-07-17 21:13:00 -0300

    Add files via upload
    Switch Finalizado

--- 3-InstruccionSwitch/jsInstruccionSwitch(6).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(6).js
@@ -2,22 +2,43 @@ function mostrar()
 {
 //tomo la edad  
 var laHora = document.getElementById('hora').value;
-var mensaje
-switch("laHora"){
+
+switch(laHora){
 
     case "7" :
     case "8" :
     case "9" :
     case "10":
     case "11":
-    mensaje="Es de mañana";
+        mensaje="Es de mañana";
 
     break;
-    
 
+    case "12":
+    case "13":
+    case "14":
+    case "15":
+    case "16":
+    case "17":
+    case "18":
+    case "19":
+         mensaje="Es de tarde";
 
+    break;
 
-
-}
-
+    case "20" :
+    case "21":
+    case "22":
+    case "23":
+    case "24":
+    case "01":
+    case "02":
+    case "03":
+    case "04":
+    case "05":
+    case "06":
+            mensaje="Es de noche";
+    break;
+    }
+    alert(mensaje)
 }//FIN DE LA FUNCIÓN
